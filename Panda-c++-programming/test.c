9fc7fb16766c5efc8b9c0ec214583eb5c669b87b
pandas-love <jaymujumderiubat@gmail.com>
2023-05-24 06:36:15 +0600

    panda-c++-programming

--- Panda-c++-programming/test.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    printf("I love u kiptuu\n");
-    printf("Hello");
-    return 0;
-}
---
+++ Panda-c++-programming/test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=11;
+    while(n>0)
+    {
+        printf("I love u kiptuu.\n");n--;
+    }
+    return 0;
+}
