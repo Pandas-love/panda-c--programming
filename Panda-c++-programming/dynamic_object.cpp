14821359946efc5cbf05e556d28cea8a1add46d6
pandas-love <jaymujumderiubat@gmail.com>
2023-05-31 08:08:27 +0600

    panda-c++-programming

--- Panda-c++-programming/dynamic_object.cpp
+++ Panda-c++-programming/dynamic_object.cpp
@@ -1,30 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 class myclass
 {
-    public:
+public:
     int age;
     int number;
     char section;
-    myclass(int,int,char);
+    myclass(int, int, char);
     void display();
 };
-myclass::myclass(int a,int n,char s)
+myclass::myclass(int a, int n, char s)
 {
-    age=a;
-    number=n;
-    section=s;
+    age = a;
+    number = n;
+    section = s;
 }
 void myclass::display()
 {
-    cout<<age<<endl;
-    cout<<"0"<<number<<endl;
-    cout<<section<<endl;
+    cout << age << endl;
+    cout << "0" << number << endl;
+    cout << section << endl;
 }
 int main()
 {
-    myclass *st=new myclass(10,1920476165,'D');
+    myclass *st = new myclass(10, 1920476165, 'D');
     st->display();
+    delete st;
     return 0;
 }
