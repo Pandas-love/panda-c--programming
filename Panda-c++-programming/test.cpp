1c9d1e4472d8fea1370508d7763c564ae83fa69d
pandas-love <jaymujumderiubat@gmail.com>
2023-06-07 07:59:22 +0600

    panda-c++0programming

--- Panda-c++-programming/test.cpp
+++ Panda-c++-programming/test.cpp
@@ -1,35 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class people
+class mai
 {
 public:
-    string name;
-    int peopleclass;
-    char section;
-    int id;
-    int mathmark;
-    int englishmark;
+    int a;
+    int b;
+    int c;
 };
+
+int getdeterminant(int a11,int a12,int a21,int a22)
+{
+    int temp=(a11*a22)-(a12*a21);
+    return temp;
+}
+int getx(int a11,int a12,int a21,int a22)
+{
+    int temp=(a11*a22)-(a12*a21);
+    return temp;
+}
+int gety(int a11,int a12,int a21,int a22)
+{
+    int temp=-(a12*a21)+(a11*a22);
+    return temp;
+}
 
 int main()
 {
     int n;
     cin >> n;
-    people mai[n];
-    for (int i = 0; i < n; i++)
-    {
-        cin >> mai[i].name;
-        cin >> mai[i].peopleclass;
-        cin >> mai[i].section;
-        cin >> mai[i].id;
-        cin >> mai[i].mathmark;
-        cin >> mai[i].englishmark;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        cout<<mai[i].name<<mai[i].peopleclass<<endl;
-    }
+    mai a[n];
     
+    for (int i = 0; i < n; i++)
+    {
+        cin>>a[i].a;
+        cin>>a[i].b;
+        cin>>a[i].c;
+    }
+    int d=getdeterminant(a[0].a,a[0].b,a[1].a,a[1].b);
+    int x=getx(a[0].c,a[0].b,a[1].c,a[1].b)/d;
+    int y=gety(a[0].a,a[0].c,a[1].a,a[1].c)/d;
+    cout<<"Value of X: "<<x<<" and"<<" Value of Y: "<<y<<endl;
     return 0;
 }
